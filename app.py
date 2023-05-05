4f42aff7a73fc95090ebcebfeeeec0e024a3e609
Martijn Zijl <martijn.zijl.3000@gmail.com>
2023-05-05 11:18:07 +0200

    formatter black

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import tkinter as tk 
+import tkinter as tk
 import socket
 import json
 import time
 from datetime import timedelta
 from select import select
 from tkinter import ttk
+
 # from view_model import *
 from PIL import ImageTk, Image
 from sys import platform
@@ -24,8 +25,8 @@ LINE_NORMAL = 0
 LINE_HIGHLIGHT = 1
 LINE_TITLE = 2
 
-LARGEFONT =("ChicagoFLF", 90) 
-MED_FONT =("ChicagoFLF", 70) 
+LARGEFONT = ("ChicagoFLF", 90)
+MED_FONT = ("ChicagoFLF", 70)
 SCALE = 1
 SPOT_GREEN = "#1DB954"
 SPOT_BLACK = "#191414"
@@ -52,57 +53,226 @@ last_button = -1
 last_interaction = time.time()
 screen_on = True
 
-class tkinterApp(tk.Tk): 
-      
-    # __init__ function for class tkinterApp  
-    def __init__(self, *args, **kwargs):  
+
+class tkinterApp(tk.Tk):
+    # __init__ function for class tkinterApp
+    def __init__(self, *args, **kwargs):
         global LARGEFONT, MED_FONT, SCALE
-        # __init__ function for class Tk 
+        # __init__ function for class Tk
         tk.Tk.__init__(self, *args, **kwargs)
 
-        if (platform == 'darwin'):
+        if platform == "darwin":
             self.geometry("320x240")
             SCALE = 0.3
         else:
-            self.attributes('-fullscreen', True)
+            self.attributes("-fullscreen", True)
             SCALE = self.winfo_screenheight() / 930
 
-        LARGEFONT =("ChicagoFLF", int(72 * SCALE))
-        MED_FONT =("ChicagoFLF", int(52 * SCALE))
-        # creating a container 
-        container = tk.Frame(self)   
-        container.pack(side = "top", fill = "both", expand = True)  
-   
-        container.grid_rowconfigure(0, weight = 1) 
-        container.grid_columnconfigure(0, weight = 1) 
-   
-        # initializing frames to an empty array 
-        self.frames = {}   
-   
-        # iterating through a tuple consisting 
-        # of the different page layouts 
-   
-    # to display the current frame passed as 
-    # parameter 
-    def show_frame(self, cont): 
-        frame = self.frames[cont] 
-        frame.tkraise() 
+        LARGEFONT = ("ChicagoFLF", int(72 * SCALE))
+        MED_FONT = ("ChicagoFLF", int(52 * SCALE))
+        # creating a container
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
 
-class Rendering():
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        # initializing frames to an empty array
+        self.frames = {}
+
+        # iterating through a tuple consisting
+        # of the different page layouts
+
+    # to display the current frame passed as
+    # parameter
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class Rendering:
     def __init__(self, type):
         self.type = type
 
     def unsubscribe(self):
         pass
 
-class LineItem():
-    def __init__(self, title = "", line_type = LINE_NORMAL, show_arrow = False):
+
+class LineItem:
+    def __init__(self, title="", line_type=LINE_NORMAL, show_arrow=False):
         self.title = title
         self.line_type = line_type
         self.show_arrow = show_arrow
-        
-class MenuPage():
-    def __init__(self, header, previous_page, has_sub_page, is_title = False):
+
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.green_arrow_image = ImageTk.PhotoImage(
+            flattenAlpha(Image.open("pod_arrow_grn.png"))
+        )
+        self.black_arrow_image = ImageTk.PhotoImage(
+            flattenAlpha(Image.open("pod_arrow_blk.png"))
+        )
+        self.empty_arrow_image = ImageTk.PhotoImage(
+            flattenAlpha(Image.open("pod_arrow_empty.png"))
+        )
+        self.play_image = ImageTk.PhotoImage(flattenAlpha(Image.open("pod_play.png")))
+        self.pause_image = ImageTk.PhotoImage(flattenAlpha(Image.open("pod_pause.png")))
+        self.space_image = ImageTk.PhotoImage(flattenAlpha(Image.open("pod_space.png")))
+        self.wifi_image = ImageTk.PhotoImage(flattenAlpha(Image.open("pod_wifi.png")))
+        self.configure(bg=SPOT_BLACK)
+        header_container = tk.Canvas(
+            self, bg=SPOT_BLACK, highlightthickness=0, relief="ridge"
+        )
+        header_container.grid(sticky="we")
+        self.header_label = tk.Label(
+            header_container,
+            text="sPot",
+            font=LARGEFONT,
+            background=SPOT_BLACK,
+            foreground=SPOT_GREEN,
+        )
+        self.header_label.grid(sticky="we", column=1, row=0, padx=(0, 10))
+        self.play_indicator = tk.Label(
+            header_container, image=self.space_image, background=SPOT_BLACK
+        )
+        self.play_indicator.grid(sticky="w", column=0, row=0, padx=(70 * SCALE, 0))
+        self.wifi_indicator = tk.Label(
+            header_container, image=self.space_image, background=SPOT_BLACK
+        )
+        self.wifi_indicator.grid(sticky="w", column=2, row=0, padx=(0, 90 * SCALE))
+        header_container.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        divider = tk.Canvas(self)
+        divider.configure(
+            bg=SPOT_GREEN,
+            height=DIVIDER_HEIGHT,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge",
+        )
+        divider.grid(row=1, column=0, sticky="we", pady=10, padx=(10, 30))
+        contentFrame = tk.Canvas(
+            self, bg=SPOT_BLACK, highlightthickness=0, relief="ridge"
+        )
+        contentFrame.grid(row=2, column=0, sticky="nswe")
+        self.grid_rowconfigure(2, weight=1)
+        listFrame = tk.Canvas(contentFrame)
+        listFrame.configure(bg=SPOT_BLACK, bd=0, highlightthickness=0)
+        listFrame.grid(row=0, column=0, sticky="nsew")
+        contentFrame.grid_rowconfigure(0, weight=1)
+        contentFrame.grid_columnconfigure(0, weight=1)
+
+        # scrollbar
+        self.scrollFrame = tk.Canvas(contentFrame)
+        self.scrollFrame.configure(
+            bg=SPOT_BLACK,
+            width=int(50 * SCALE),
+            bd=0,
+            highlightthickness=4,
+            highlightbackground=SPOT_GREEN,
+        )
+        self.scrollBar = tk.Canvas(
+            self.scrollFrame, bg=SPOT_GREEN, highlightthickness=0, width=int(20 * SCALE)
+        )
+        self.scrollBar.place(
+            in_=self.scrollFrame,
+            relx=0.5,
+            y=int(10 * SCALE),
+            anchor="n",
+            relwidth=0.6,
+            relheight=0.9,
+        )
+        self.scrollFrame.grid(row=0, column=1, sticky="ns", padx=(0, 30), pady=(0, 10))
+
+        self.listItems = []
+        self.arrows = []
+        for x in range(6):
+            item = tk.Label(
+                listFrame,
+                text=" " + str(x),
+                justify=tk.LEFT,
+                anchor="w",
+                font=LARGEFONT,
+                background=SPOT_BLACK,
+                foreground=SPOT_GREEN,
+                padx=(30 * SCALE),
+            )
+            imgLabel = tk.Label(
+                listFrame, image=self.green_arrow_image, background=SPOT_BLACK
+            )
+            imgLabel.image = self.green_arrow_image
+            imgLabel.grid(row=x, column=1, sticky="nsw", padx=(0, 30))
+            item.grid(row=x, column=0, sticky="ew", padx=(10, 0))
+            self.listItems.append(item)
+            self.arrows.append(imgLabel)
+        listFrame.grid_columnconfigure(0, weight=1)
+        # listFrame.grid_columnconfigure(1, weight=1)
+
+    def show_scroll(self, index, total_count):
+        scroll_bar_y_rel_size = max(0.9 - (total_count - MENU_PAGE_SIZE) * 0.06, 0.03)
+        scroll_bar_y_raw_size = scroll_bar_y_rel_size * self.scrollFrame.winfo_height()
+        percentage = index / (total_count - 1)
+        offset = ((1 - percentage) * (scroll_bar_y_raw_size + int(20 * SCALE))) - (
+            scroll_bar_y_raw_size + int(10 * SCALE)
+        )
+        self.scrollBar.place(
+            in_=self.scrollFrame,
+            relx=0.5,
+            rely=percentage,
+            y=offset,
+            anchor="n",
+            relwidth=0.66,
+            relheight=scroll_bar_y_rel_size,
+        )
+        self.scrollFrame.grid(row=0, column=1, sticky="ns", padx=(0, 30), pady=(0, 10))
+
+    def hide_scroll(self):
+        self.scrollFrame.grid_forget()
+
+    def set_header(self, header, now_playing=None, has_wifi=False):
+        truncd_header = header if len(header) < 20 else header[0:17] + "..."
+        self.header_label.configure(text=truncd_header)
+        play_image = self.space_image
+        if now_playing is not None:
+            play_image = (
+                self.play_image if now_playing["is_playing"] else self.pause_image
+            )
+        self.play_indicator.configure(image=play_image)
+        self.play_indicator.image = play_image
+        wifi_image = self.wifi_image if has_wifi else self.space_image
+        self.wifi_indicator.configure(image=wifi_image)
+        self.wifi_indicator.image = wifi_image
+
+    def set_list_item(self, index, text, line_type=LINE_NORMAL, show_arrow=False):
+        bgColor = SPOT_GREEN if line_type == LINE_HIGHLIGHT else SPOT_BLACK
+        txtColor = (
+            SPOT_BLACK
+            if line_type == LINE_HIGHLIGHT
+            else (SPOT_GREEN if line_type == LINE_NORMAL else SPOT_WHITE)
+        )
+        truncd_text = text if len(text) < 17 else text[0:15] + "..."
+        self.listItems[index].configure(
+            background=bgColor, foreground=txtColor, text=truncd_text
+        )
+        arrow = self.arrows[index]
+        arrow.grid(row=index, column=1, sticky="nsw", padx=(0, 30))
+        arrowImg = (
+            self.empty_arrow_image
+            if not show_arrow
+            else (
+                self.black_arrow_image
+                if line_type == LINE_HIGHLIGHT
+                else self.green_arrow_image
+            )
+        )
+        arrow.configure(background=bgColor, image=arrowImg)
+        arrow.image = arrowImg
+
+
+class MenuPage:
+    def __init__(self, header, previous_page, has_sub_page, is_title=False):
         self.index = 0
         self.page_start = 0
         self.header = header
@@ -124,7 +294,7 @@ class MenuPage():
 
     def nav_play(self):
         pass
-    
+
     def get_index_jump_up(self):
         return 1
 
@@ -133,19 +303,19 @@ class MenuPage():
 
     def nav_up(self):
         jump = self.get_index_jump_up()
-        if(self.index >= self.total_size() - jump):
+        if self.index >= self.total_size() - jump:
             return
-        if (self.index >= self.page_start + MENU_PAGE_SIZE - jump):
+        if self.index >= self.page_start + MENU_PAGE_SIZE - jump:
             self.page_start = self.page_start + jump
         self.index = self.index + jump
 
     def nav_down(self):
         jump = self.get_index_jump_down()
-        if(self.index <= (jump - 1)):
+        if self.index <= (jump - 1):
             return
-        if (self.index <= self.page_start + (jump - 1)):
+        if self.index <= self.page_start + (jump - 1):
             self.page_start = self.page_start - jump
-            if (self.page_start == 1):
+            if self.page_start == 1:
                 self.page_start = 0
         self.index = self.index - jump
 
@@ -159,20 +329,31 @@ class MenuPage():
         lines = []
         total_size = self.total_size()
         for i in range(self.page_start, self.page_start + MENU_PAGE_SIZE):
-            if (i < total_size):
+            if i < total_size:
                 page = self.page_at(i)
-                if (page is None) :
+                if page is None:
                     lines.append(LineItem())
                 else:
-                    line_type = LINE_TITLE if page.is_title else \
-                        LINE_HIGHLIGHT if i == self.index else LINE_NORMAL
+                    line_type = (
+                        LINE_TITLE
+                        if page.is_title
+                        else LINE_HIGHLIGHT
+                        if i == self.index
+                        else LINE_NORMAL
+                    )
                     lines.append(LineItem(page.header, line_type, page.has_sub_page))
             else:
                 lines.append(LineItem())
-        return MenuRendering(lines=lines, header=self.header, page_start=self.index, total_count=total_size)
+        return MenuRendering(
+            lines=lines,
+            header=self.header,
+            page_start=self.index,
+            total_count=total_size,
+        )
+
 
 class MenuRendering(Rendering):
-    def __init__(self, header = "", lines = [], page_start = 0, total_count = 0):
+    def __init__(self, header="", lines=[], page_start=0, total_count=0):
         super().__init__(MENU_RENDER_TYPE)
         self.lines = lines
         self.header = header
@@ -181,49 +362,88 @@ class MenuRendering(Rendering):
         self.now_playing = []
         self.has_internet = True
 
+
 class RootPage(MenuPage):
     def __init__(self, previous_page):
         super().__init__("Ari's ipod", previous_page, has_sub_page=True)
-        self.pages = [
-         "Playlists",
-         "Now playing"
-        ]
+        self.pages = ["Playlists", "Now playing"]
         self.index = 0
         self.page_start = 0
-    
+
     def get_pages(self):
         return self.pages
-    
+
     def total_size(self):
         return len(self.get_pages())
 
     def page_at(self, index):
         return self.get_pages()[index]
 
+
 page = RootPage(None)
-app = tkinterApp() 
+app = tkinterApp()
+
+
+def flattenAlpha(img):
+    global SCALE
+    [img_w, img_h] = img.size
+    img = img.resize((int(img_w * SCALE), int(img_h * SCALE)), Image.ANTIALIAS)
+    alpha = img.split()[-1]  # Pull off the alpha layer
+    ab = alpha.tobytes()  # Original 8-bit alpha
+    checked = []  # Create a new array to store the cleaned up alpha layer bytes
+    # Walk through all pixels and set them either to 0 for transparent or 255 for opaque fancy pants
+    transparent = 50  # change to suit your tolerance for what is and is not transparent
+    p = 0
+    for pixel in range(0, len(ab)):
+        if ab[pixel] < transparent:
+            checked.append(0)  # Transparent
+        else:
+            checked.append(255)  # Opaque
+        p += 1
+    mask = Image.frombytes("L", img.size, bytes(checked))
+    img.putalpha(mask)
+    return img
+
 
 def screen_sleep():
     global screen_on
     screen_on = False
-    os.system('xset -display :0 dpms force off')
+    os.system("xset -display :0 dpms force off")
+
 
 def screen_wake():
     global screen_on
     screen_on = True
-    os.system('xset -display :0 dpms force on')
+    os.system("xset -display :0 dpms force on")
+
+
+def render_menu(app, menu_render):
+    app.show_frame(StartPage)
+    page = app.frames[StartPage]
+    if menu_render.total_count > MENU_PAGE_SIZE:
+        page.show_scroll(menu_render.page_start, menu_render.total_count)
+    else:
+        page.hide_scroll()
+    for i, line in enumerate(menu_render.lines):
+        page.set_list_item(
+            i, text=line.title, line_type=line.line_type, show_arrow=line.show_arrow
+        )
+    page.set_header(
+        menu_render.header, menu_render.now_playing, menu_render.has_internet
+    )
 
 
 def render(app, render):
+    render_menu(app, render)
     pass
-    
+
 
 def app_main_loop():
     global app, page, loop_count, last_interaction, screen_on
     try:
         loop_count += 1
-        if (loop_count >= 300):
-            if (time.time() - last_interaction > SCREEN_TIMEOUT_SECONDS and screen_on):
+        if loop_count >= 300:
+            if time.time() - last_interaction > SCREEN_TIMEOUT_SECONDS and screen_on:
                 screen_sleep()
             render(app, page.render())
             loop_count = 0
